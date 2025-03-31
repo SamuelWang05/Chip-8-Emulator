1fcf06161316b5656f32f949ac026beb0810b7a7
SamuelWang05 <samuel.t.wang@vanderbilt.edu>
2025-03-31 00:38:14 -0500

    Batman

---
+++ include/chip8.h
@@ -0,0 +1,52 @@
+/**
+* CPU core implementation for Chip-8 interpreter
+*
+* Implementation of 35 different op codes (2 bytes long)
+* More information about Chip-8 can be found on the wikipedia page
+* https://en.wikipedia.org/wiki/CHIP-8#Virtual_machine_description
+*
+* This code is mostly based on the guidelines provided by Laurence Muller, but
+* with my own implementation for the code
+*
+* His article can be found here:
+* https://multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
+*/
+
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#include <stack>
+
+class chip8 {
+public:
+
+
+private:
+    unsigned short opcode;          // Op codes are 2 bytes long
+    unsigned char memory[4096];     // 4k memory total
+    unsigned char V[16];            // 15 8-bit general purpose registers, 16th used for carry flag
+    unsigned short I;               // Index register
+    unsigned short pc;              // Program counter
+    /**
+     * Systems memory map:
+     * 0x000-0x1FF - Chip 8 interpreter (contains font set in emu)
+     * 0x050-0x0A0 - Used for the built-in 4x5 pixel font set (0-F)
+     * 0x200-0xFFF - Program ROM and work RAM
+     */
+
+    // Graphics
+    unsigned char gfx[64 * 32];     // Graphics written on a 64 x 32 pixel screen. Only black/white colors supported
+
+    // Two timer registers, count @ 60 Hz. Set > 0 --> will count down to 0
+    unsigned char delay_timer;
+    unsigned char sound_timer; // Will sound whenever timer reaches 0
+
+    // Stack - remember current location before a jump is performed by PC
+    unsigned short stack[16];
+    unsigned short sp; // Stack pointer
+
+    // Keyboard (hex based)
+    unsigned char key[16];
+};
+
+#endif //CHIP8_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+int main() {
+
+    if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO))
+
+    return 0;
+}
